4710f1e5d78b8b10ac987b3a451620327dbc418a
Argha Guha <arghaguha3001@gmail.com>
2025-04-20 17:34:17 +0530

    Set up Redux store and refactoring

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("argha@gmail.com");
-  const [password, setPassword] = useState("Argha@123");
+  const navigate = useNavigate();
+
+  const [emailId, setEmailId] = useState("azim@gmail.com");
+  const [password, setPassword] = useState("Azim@123");
+  const dispatch = useDispatch();
 
   const handleLogin = async () => {
     try{
-      const res = await axios.post("http://localhost:3001/login", {
+      const res = await axios.post( BASE_URL+"/login", { 
         emailId: emailId,
         password: password
       },{
         withCredentials: true
       })
+      dispatch(addUser(res.data));
+      return navigate("/")
     }catch(err){
       console.error(err);
     } 
